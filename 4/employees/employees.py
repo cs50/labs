b6c0eb01e045079a945f8d046220a084b4b388df
David J. Malan <malan@harvard.edu>
2019-04-25 23:21:46 -0400

    removed distro

--- 4/employees/employees.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open("employees.csv", "r") as file:
-    print("Manager,LastName,FirstName")
-    reader = csv.DictReader(file)
-    for row in reader:
-        print(row["Manager"], row["LastName"], row["FirstName"], sep=",")
