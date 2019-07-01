d0f179dfbc27c55d6becdeeeb4593fb133d74471
Brian Yu <brianyu28@gmail.com>
2019-07-01 17:56:54 -0400

    Remove temporary dir

--- finance/application.py
+++ finance/application.py
@@ -27,7 +27,6 @@ def after_request(response):
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
