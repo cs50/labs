fec7e017391ea9c6ae3e1c1603962c85c1599151
Brian Yu <brian@cs.harvard.edu>
2020-09-19 13:45:36 -0400

    Add check for answering all questions

--- sort/__init__.py
+++ sort/__init__.py
@@ -8,6 +8,13 @@ def exists():
     check50.exists("answers.txt")
 
 @check50.check(exists)
+def answers():
+    """answers all questions"""
+    content = open("answers.txt", "r").read()
+    if "TODO" in content:
+        raise check50.Failure("Not all questions answered.")
+
+@check50.check(exists)
 def sorts():
     """correctly identifies each sort"""
 
