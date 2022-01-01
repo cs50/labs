2d5b7fe3dc73a2d6fbc43523b7d8f7361846931d
Rongxin Liu <rongxinliu.dev@gmail.com>
2022-01-01 02:20:37 -0500

    updated checks for birthdays and trivia

---
+++ birthdays/__init__.py
@@ -0,0 +1,6 @@
+import check50
+
+@check50.check()
+def exists():
+    """Birthdays submitted"""
+    pass
---
+++ trivia/__init__.py
@@ -0,0 +1,6 @@
+import check50
+
+@check50.check()
+def exists():
+    """Trivia submitted"""
+    pass
